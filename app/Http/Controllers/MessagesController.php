1d701e38c6ceb80f10c7e3fbf548fc79f6a68833
kontrasenyas <alcantarajosephc@gmail.com>
2016-08-13 20:38:26 +0800

    message noti

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -7,21 +7,120 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use Auth;
 use DB;
+use App\Product;
+use App\BookingNotification;
+use App\Booking;
 
 class MessagesController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-    	$user = Auth::user();
-    	$userEmail = $user->email;
-    	$products = DB::table('products');
-    	$messages = $products
-    		->join('bookings', 'products.id', '=', 'bookings.ProductID')
-    		->where('EmailAddress', '=', ''. $userEmail . '')
+        $user = Auth::user();
+        $userEmail = $user->email;
+        $products = DB::table('products');
+        $messages = $products
+            ->join('bookings', 'products.id', '=', 'bookings.ProductID')
+            ->where('EmailAddress', '=', ''. $userEmail . '')
             ->orderBy("bookings.DateCreated", "desc")
             //->orWhere('description', 'LIKE', '%'. $searchterm .'%')
             //->orWhere('brand', 'LIKE', '%'. $searchterm .'%')
             ->get();
         return view('messages')->with('messages', $messages);;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = Auth::user();
+        $product = Product::find($id);
+        $BookingNotification = new BookingNotification;
+
+        $BookingNotification->Code = $request->input('product_code');
+
+        //$BookingNotification->save();
+
+        return $BookingNotification;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = Auth::user();
+        
+        //return $Code;        
+
+        $Booking = new Booking;
+
+        $Booking = Booking::find($id);
+
+        $Booking->isReadOwner = '1';
+        $Booking->save();
+
+        
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    public function isRead($id)
+    {
+        $product = Product::find($id);
+        return $product;
     }
 }
--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -11,12 +11,19 @@
 
                 <div class="panel-body">
                     @foreach($messages as $messages)
+                     {!!Form::model($messages, [
+                            'method'=>'patch',
+                            'route'=>['messages.update', $messages->id]
+                            ])!!}
                         <div class="panel panel-default col-md-12" style="padding-right: 0px; padding-left: 0px; !important">
                             <div class="panel-heading col-md-12 btn-group" role="group">
                                 <a href="{{ url('product', $messages->ProductID) }}" class="btn btn-secondary">Someone booked your post: <b>{{$messages->Title}}</b></a>
-                                <a href="{{route('messages.update', $messages->ProductID)}}" class="btn">View</a>
+                                {{-- <a href="{{route('messages.edit', $messages->ProductID)}}" class="btn">View</a>
+                                <a href="{{action('MessagesController@update', $messages->Code)}}">Link name/Embedded Button</a> --}}
+                                {!!Form::submit('View', array('class' => 'btn btn-primary'))!!}
                             </div>
-                        
+                            <input id="product_codeproduct_code" name="product_code" type="hidden" value="{{$messages->id}}">                        
+                    {!!Form::close()!!}
                     @endforeach
                 </div>
             </div>
